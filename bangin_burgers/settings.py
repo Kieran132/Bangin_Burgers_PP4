615a8e00eac22b1adc4b4d8ea8387c6494075c3f
Kieran132 <kiemclean@hotmail.co.uk>
2023-02-28 10:08:02 +0000

    Ammend spelling on allowed host url

--- bangin_burgers/settings.py
+++ bangin_burgers/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bangin-burgers-pp4.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['bangin-burgers-pp4.herokuapp.com', 'localhost']
 
 
 # Application definition
