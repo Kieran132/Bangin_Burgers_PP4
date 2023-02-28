3f2fa39a2987169930e1f661f1c112dd164c495e
Kieran132 <kiemclean@hotmail.co.uk>
2023-02-28 10:38:28 +0000

    Add model to admin page

--- order/admin.py
+++ order/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import FoodItem
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(FoodItem)
+@admin.register(FoodItem)
+class FoodAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'slug')
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name', )}
+    summernote_fields = ('description')
 
