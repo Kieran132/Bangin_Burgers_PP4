19481b42542941df84ee1a0529199655ffb6a96a
Kieran132 <kiemclean@hotmail.co.uk>
2023-02-28 10:42:47 +0000

    Create FoodList view in views.py

--- order/views.py
+++ order/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import FoodItem
 
-# Create your views here.
+class FoodList(generic.ListView):
+    model = FoodItem
+    queryset = FoodItem.objects.filter(status=1)
+    template_name = 'order.html'
+    paginate_by = 6
+
