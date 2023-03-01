4b6ac470293af5e3f48f16b0b143cb7c382065b7
Kieran132 <kiemclean@hotmail.co.uk>
2023-03-01 11:51:36 +0000

    Add test template and urls

--- order/views.py
+++ order/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import FoodItem
 
+
 class FoodList(generic.ListView):
     model = FoodItem
-    queryset = FoodItem.objects.filter(status=1)
+    queryset = FoodItem
     template_name = 'order.html'
     paginate_by = 6
 
